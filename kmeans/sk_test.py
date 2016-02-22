ccd258c5f64c2fba291980ed74eba053fd6dbd96
Saurabh7 <saurabh.mahindre@gmail.com>
2016-02-22 23:15:41 +0530

    kmeans scripts

--- kmeans/sk_test.py
+++ kmeans/sk_test.py
@@ -3,14 +3,14 @@ from sklearn.cluster import KMeans
 import numpy as np
 from time import time
 from sklearn.preprocessing import Imputer
-imp = Imputer(missing_values="NaN", strategy='mean', axis=0)
+#imp = Imputer(missing_values="NaN", strategy='mean', axis=0)
 
-csv = np.genfromtxt('census_50k.csv', delimiter=",")
+csv = np.genfromtxt('student.csv', delimiter=",")
 #imp.fit(csv)
 #imp.transform(csv)
 #print np.any(np.isnan(csv))
 t0 = time()
-k = KMeans(init='random', n_clusters=1000, n_init=1)
+k = KMeans(init='random', n_clusters=100, n_init=1)
 k.fit(csv)
 t = time()-t0
 print t
